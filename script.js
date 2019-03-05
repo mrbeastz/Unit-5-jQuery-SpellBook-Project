8d917f7466194519f75b3f0e31dd09eae77ef834
mrbeastz <43611204+mrbeastz@users.noreply.github.com>
2019-03-05 09:41:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(" #shrink ").click(function(){
+ $("#dragon").slideToggle();
+});
+
+
+
+//Spell #2
+
+$(" #fold   ").click(function(){
+ $("#water").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("  #hide   ").click(function(){
+ $("#god").fadeToggle();
+});
+
+
+
